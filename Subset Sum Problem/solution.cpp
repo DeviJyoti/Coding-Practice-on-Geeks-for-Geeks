19a02a5f02b2ca24c287560fd864b6fa357254b6
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2023-08-13 15:16:23 +0530

    Create solution.cpp

---
+++ Subset Sum Problem/solution.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    bool isSubsetSum(vector<int>arr, int w){
+        // code here 
+         int n =arr.size();
+        int t[n+1][w+1];
+        
+        //inilialize the matrix
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<w+1;j++){
+                if(i==0) t[i][j]=0;
+                if(j==0) t[i][j]=1;
+            }
+        }
+       
+       //iterative code
+       for(int i=1;i<n+1;i++){
+           for(int j=1;j<w+1;j++){
+               if(arr[i-1]<=j){
+                   t[i][j]=(t[i-1][j-arr[i-1]] || t[i-1][j]);
+               }else{
+                   t[i][j]=t[i-1][j];
+               }
+           }
+       }
+       return t[n][w];
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, sum;
+        cin >> N;
+        vector<int> arr(N);
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        cin >> sum;
+        
+        Solution ob;
+        cout << ob.isSubsetSum(arr, sum) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
